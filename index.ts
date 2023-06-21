b38f813f93fa40c6bf38e51a6f37ea560571f986
bernabe iglesias <jcberna@outlook.com>
2023-06-20 23:24:33 -0500

    server Commit

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import Server from "./classes/server";
 const server = new Server();
 
 server.start( () => {
-    console.log('corriendo en el puerto:',server.port);
+    console.log('corriendo en el puert:',server.port);
 });
